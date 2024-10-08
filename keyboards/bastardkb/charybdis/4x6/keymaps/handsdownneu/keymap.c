9993fdf6dd021c4663ffcd119e34bb3cf63607a0
Frank Saul <frank.saul@gmail.com>
2024-10-08 23:55:50 +0200

    add cheat sheet to comment

--- keyboards/bastardkb/charybdis/4x6/keymaps/handsdownneu/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/handsdownneu/keymap.c
@@ -14,6 +14,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+// ********************************************************************
+// https://jayliu50.github.io/qmk-cheatsheet/
+// ********************************************************************
+
 #include QMK_KEYBOARD_H
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
