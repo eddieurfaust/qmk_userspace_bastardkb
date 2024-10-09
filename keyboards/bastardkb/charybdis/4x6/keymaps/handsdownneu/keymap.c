b1ba9217979913b60a9da49e668207260ae2e2f5
Frank Saul <frank.saul@gmail.com>
2024-10-09 22:10:06 +0200

    move buttons on thumb cluster around

--- keyboards/bastardkb/charybdis/4x6/keymaps/handsdownneu/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/handsdownneu/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        XXXXXXX,    PT_X,    KC_C,    KC_L,    KC_D,    KC_G,    XXXXXXX,    KC_U,    KC_O,    KC_Z,    KC_K, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                 NAVIGATION, KC_LSFT,   XXXXXXX,     KC_SPC, SYMBOL,
+                                XXXXXXX, KC_LSFT, NAVIGATION,   SYMBOL, KC_SPC,
                                            XXXXXXX, XXXXXXX,     NUMBERS
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
