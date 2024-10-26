67f7121155aa0d2f1e48e0b1b822700a3c193479
Frank Saul <frank.saul@gmail.com>
2024-10-26 23:44:12 +0200

    try tap dance example

--- keyboards/bastardkb/charybdis/4x6/keymaps/handsdownneu/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/handsdownneu/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        XXXXXXX,    KC_R,    KC_S,    KC_N,    KC_T,    KC_B,    KC_COMMA,   KC_A,    KC_E,    KC_I,    KC_H, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       XXXXXXX,    PT_X,    KC_C,    KC_L,    KC_D,    KC_G,    KC_SLASH,    KC_U,    KC_O,    KC_Z,    KC_K, XXXXXXX,
+       XXXXXXX,    PT_X,    KC_C,    KC_L,    KC_D,    KC_G,    TD(CT_CLN),    KC_U,    KC_O,    KC_Z,    KC_K, XXXXXXX,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
                                NAVIGATION, KC_LSFT, XXXXXXX,  KC_SPC, SYMBOL,
                                            XXXXXXX, XXXXXXX,     NUMBERS
@@ -178,6 +178,65 @@ combo_t key_combos[] = {
     COMBO(left_angle_bracket, KC_NONUS_BACKSLASH),
     COMBO(right_angle_bracket, LSFT(KC_NONUS_BACKSLASH)),
     COMBO(num_layer, NUMBERS)
+};
+
+// ********************************************************************
+// TAP DANCE
+// ********************************************************************
+
+// taken from example 3: https://docs.qmk.fm/features/tap_dance#examples
+typedef struct {
+    uint16_t tap;
+    uint16_t hold;
+    uint16_t held;
+} tap_dance_tap_hold_t;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    tap_dance_action_t *action;
+
+    switch (keycode) {
+        case TD(CT_CLN):  // list all tap dance keycodes with tap-hold configurations
+            action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
+            if (!record->event.pressed && action->state.count && !action->state.finished) {
+                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+                tap_code16(tap_hold->tap);
+            }
+    }
+    return true;
+}
+
+void tap_dance_tap_hold_finished(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (state->pressed) {
+        if (state->count == 1
+#ifndef PERMISSIVE_HOLD
+            && !state->interrupted
+#endif
+        ) {
+            register_code16(tap_hold->hold);
+            tap_hold->held = tap_hold->hold;
+        } else {
+            register_code16(tap_hold->tap);
+            tap_hold->held = tap_hold->tap;
+        }
+    }
+}
+
+void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (tap_hold->held) {
+        unregister_code16(tap_hold->held);
+        tap_hold->held = 0;
+    }
+}
+
+#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
+    { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
+
+tap_dance_action_t tap_dance_actions[] = {
+    [CT_CLN] = ACTION_TAP_DANCE_TAP_HOLD(KC_COLN, KC_SCLN),
 };
 
 // clang-format on
